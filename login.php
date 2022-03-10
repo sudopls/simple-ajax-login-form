390ea49bf12c778585ccc2975e96ec40802e79e9
Fredrik <fl@kommandokringla.se>
2022-03-10 13:03:07 +0100

    changed cdn for jquery

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <form id="loginform" method="post">
